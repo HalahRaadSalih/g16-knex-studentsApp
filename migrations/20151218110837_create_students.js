8981873aca677700bef32240303bbf24453c99ef
HalahRaadSalih <hala.raad.salih@gmail.com>
2015-12-18 11:10:51 -0800

    created migration

---
+++ migrations/20151218110837_create_students.js
@@ -0,0 +1,11 @@
+
+exports.up = function(knex, Promise) {
+	return knex.schema.createTable('students', function(table){
+		table.increments(); // id serial primary key
+		table.string('name');
+	});
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('students');
+};
