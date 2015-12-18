36eae1432620bac7ca76bc85329880b50647c947
HalahRaadSalih <hala.raad.salih@gmail.com>
2015-12-18 11:23:37 -0800

    added knex to app.js and added route to index page

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
+var knex = require('./db/knex');
 
 //middleware
 app.set('view engine', 'ejs');
@@ -9,6 +10,14 @@ app.use(bodyParser.urlencoded({
 	extended: true}
 	));
 app.use(morgan('tiny'));
+
+app.get('/students', function(req,res){
+	knex('students').then(function(students){
+		res.render('index', {
+			students: students
+		});
+	});
+});
 
 app.listen(3000, function(){
 	console.log("knwewjfnjfnnf");
